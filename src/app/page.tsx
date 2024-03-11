eb150ba4adb7e016a924602bf0916dc263ada4b4
jamalabd <jamalabdul47@gmail.com>
2024-03-11 15:48:18 -0400

    feat: links, logo, image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,22 +6,21 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
+          Current Match&nbsp;
+          <code className={styles.code}>
+            {" "}
+            Jamal <span className={styles.vs}>VS</span> Matt{" "}
+          </code>
         </p>
         <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a href="https://muhaaryb.com/" target="_blank">
             By{" "}
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
+              src="/Muhaaryb.png"
+              alt="Muhaaryb Logo"
+              className={styles.muhaarybLogo}
+              width={50}
+              height={25}
               priority
             />
           </a>
@@ -31,63 +30,41 @@ export default function Home() {
       <div className={styles.center}>
         <Image
           className={styles.logo}
-          src="/next.svg"
+          src="/Bjj-fighters-gi.svg"
           alt="Next.js Logo"
-          width={180}
+          width={280}
           height={37}
           priority
         />
       </div>
 
       <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a href="" className={styles.card}>
           <h2>
-            Docs <span>-&gt;</span>
+            Score <span>-&gt;</span>
           </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
+          <p>Keep track of your matches scores.</p>
         </a>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a href="" className={styles.card}>
           <h2>
-            Learn <span>-&gt;</span>
+            Brackets <span>-&gt;</span>
           </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
+          <p>See current brackets.</p>
         </a>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a href="" className={styles.card}>
           <h2>
-            Templates <span>-&gt;</span>
+            Prizes <span>-&gt;</span>
           </h2>
-          <p>Explore starter templates for Next.js.</p>
+          <p> Put up grand prizes for for first, second, & third place!</p>
         </a>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a href="https://muhaaryb.com/" className={styles.card} target="_blank">
           <h2>
-            Deploy <span>-&gt;</span>
+            Shop <span>-&gt;</span>
           </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
+          <p>Shop everything you need for the next match.</p>
         </a>
       </div>
     </main>
